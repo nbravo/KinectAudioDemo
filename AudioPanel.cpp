0e0b5d87b6eab79575fd56aa18161c762c1c2989
Nicolas Bravo <nbravo@mit.edu>
2015-03-01 19:03:07 -0800

    fix some brace positions

--- AudioPanel.cpp
+++ AudioPanel.cpp
@@ -1,15 +1,7 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="AudioPanel.cpp" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
-
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "AudioPanel.h"
 
-/// <summary>
 /// Constructor
-/// </summary>
 AudioPanel::AudioPanel() : 
     m_hWnd(0),
     m_pD2DFactory(NULL), 
@@ -36,27 +28,20 @@ AudioPanel::AudioPanel() :
 {
 }
 
-/// <summary>
 /// Destructor
-/// </summary>
-AudioPanel::~AudioPanel()
-{
+AudioPanel::~AudioPanel() {
     DiscardResources();
     SafeRelease(m_pD2DFactory);
 }
 
-/// <summary>
 /// Set the window to draw to as well as the video format
 /// Implied bits per pixel is 32
-/// </summary>
 /// <param name="hWnd">window to draw to</param>
 /// <param name="pD2DFactory">already created D2D factory object</param>
 /// <param name="energyToDisplay">Number of energy samples to display at any given time.</param>
 /// <returns>S_OK on success, otherwise failure code.</returns>
-HRESULT AudioPanel::Initialize(const HWND hWnd, ID2D1Factory* pD2DFactory, const UINT energyToDisplay)
-{
-    if (NULL == pD2DFactory)
-    {
+HRESULT AudioPanel::Initialize(const HWND hWnd, ID2D1Factory* pD2DFactory, const UINT energyToDisplay) {
+    if (NULL == pD2DFactory) {
         return E_INVALIDARG;
     }
 
@@ -73,17 +58,13 @@ HRESULT AudioPanel::Initialize(const HWND hWnd, ID2D1Factory* pD2DFactory, const
     return S_OK;
 }
 
-/// <summary>
 /// Draws audio panel.
-/// </summary>
 /// <returns>S_OK on success, otherwise failure code.</returns>
-HRESULT AudioPanel::Draw()
-{
+HRESULT AudioPanel::Draw() {
     // create the resources for this draw device. They will be recreated if previously lost.
     HRESULT hr = EnsureResources();
 
-    if ( FAILED(hr) )
-    {
+    if ( FAILED(hr) ) {
         return hr;
     }
     
@@ -111,8 +92,7 @@ HRESULT AudioPanel::Draw()
     hr = m_pRenderTarget->EndDraw();
 
     // Device lost, need to recreate the render target. We'll dispose it now and retry drawing.
-    if (hr == D2DERR_RECREATE_TARGET)
-    {
+    if (hr == D2DERR_RECREATE_TARGET) {
         hr = S_OK;
         DiscardResources();
     }
@@ -120,20 +100,16 @@ HRESULT AudioPanel::Draw()
     return hr;
 }
 
-void AudioPanel::SetBeam(const float & beamAngle)
-{
-    if (m_pRenderTarget == NULL)
-    {
+void AudioPanel::SetBeam(const float & beamAngle) {
+    if (m_pRenderTarget == NULL) {
         return;
     }
 
     m_BeamNeedleTransform = D2D1::Matrix3x2F::Rotation(-beamAngle, D2D1::Point2F(0.5f,0.0f));
 }
 
-void AudioPanel::SetSoundSource(const float & soundSourceAngle, const float & soundSourceConfidence)
-{
-    if (m_pRenderTarget == NULL)
-    {
+void AudioPanel::SetSoundSource(const float & soundSourceAngle, const float & soundSourceConfidence) {
+    if (m_pRenderTarget == NULL) {
         return;
     }
 
@@ -151,10 +127,8 @@ void AudioPanel::SetSoundSource(const float & soundSourceAngle, const float & so
     m_SourceGaugeTransform = D2D1::Matrix3x2F::Rotation(-soundSourceAngle, D2D1::Point2F(0.5f,0.0f));
 }
 
-void AudioPanel::UpdateEnergy(const float *pEnergy, const UINT energyLength)
-{
-    if (m_pRenderTarget == NULL)
-    {
+void AudioPanel::UpdateEnergy(const float *pEnergy, const UINT energyLength) {
+    if (m_pRenderTarget == NULL) {
         return;
     }
 
@@ -164,8 +138,7 @@ void AudioPanel::UpdateEnergy(const float *pEnergy, const UINT energyLength)
     // Draw each energy sample as a centered vertical bar, where the length of each bar is
     // proportional to the amount of energy it represents.
     // Time advances from left to right, with current time represented by the rightmost bar.
-    for (UINT i = 0; i < min(energyLength, m_uiEnergyDisplayWidth); ++i)
-    {
+    for (UINT i = 0; i < min(energyLength, m_uiEnergyDisplayWidth); ++i) {
         const int cHalfImageHeight = m_uiEnergyDisplayHeight / 2;
 
         // Each bar has a minimum height of 1 (to get a steady signal down the middle) and a maximum height
@@ -182,17 +155,14 @@ void AudioPanel::UpdateEnergy(const float *pEnergy, const UINT energyLength)
     }
 }
 
-/// <summary>
+
 /// Dispose of Direct2d resources.
-/// </summary>
-void AudioPanel::DiscardResources()
-{
-    if (m_pEnergyBackground != NULL)
-    {
+
+void AudioPanel::DiscardResources() {
+    if (m_pEnergyBackground != NULL) {
         delete []m_pEnergyBackground;
     }
-    if (m_pEnergyForeground != NULL)
-    {
+    if (m_pEnergyForeground != NULL) {
         delete []m_pEnergyForeground;
     }
     
@@ -208,16 +178,14 @@ void AudioPanel::DiscardResources()
     SafeRelease(m_pPanelOutlineStroke);
 }
 
-/// <summary>
+
 /// Ensure necessary Direct2d resources are created
-/// </summary>
+
 /// <returns>indicates success or failure</returns>
-HRESULT AudioPanel::EnsureResources()
-{
+HRESULT AudioPanel::EnsureResources() {
     HRESULT hr = S_OK;
 
-    if (NULL == m_pRenderTarget)
-    {
+    if (NULL == m_pRenderTarget) {
         // Get the panel size
         RECT panelRect = {0};
         GetWindowRect(m_hWnd, &panelRect);
@@ -238,20 +206,16 @@ HRESULT AudioPanel::EnsureResources()
             &m_pRenderTarget
             );
 
-        if (SUCCEEDED(hr))
-        {
+        if (SUCCEEDED(hr)) {
             hr = CreateEnergyDisplay();
 
-            if (SUCCEEDED(hr))
-            {
+            if (SUCCEEDED(hr)) {
                 hr = CreateBeamGauge();
 
-                if (SUCCEEDED(hr))
-                {
+                if (SUCCEEDED(hr)) {
                     hr = CreateSourceGauge();
 
-                    if (SUCCEEDED(hr))
-                    {
+                    if (SUCCEEDED(hr)) {
                         hr = CreatePanelOutline();
                     }
                 }
@@ -259,20 +223,18 @@ HRESULT AudioPanel::EnsureResources()
         }
     }
 
-    if ( FAILED(hr) )
-    {
+    if ( FAILED(hr) ) {
         DiscardResources();
     }
 
     return hr;
 }
 
-/// <summary>
+
 /// Create oscilloscope display for audio energy data.
-/// </summary>
+
 /// <returns>S_OK on success, otherwise failure code.</returns>
-HRESULT AudioPanel::CreateEnergyDisplay()
-{
+HRESULT AudioPanel::CreateEnergyDisplay() {
     const int cBytesPerPixel = 4;
     const int cMaxPixelChannelIntensity = 0xff;
 
@@ -289,8 +251,7 @@ HRESULT AudioPanel::CreateEnergyDisplay()
     m_uiEnergyForegroundStride = cBytesPerPixel;
     m_pEnergyForeground = new BYTE[cBytesPerPixel * m_uiEnergyDisplayHeight];
     UINT *pPixels = reinterpret_cast<UINT*>(m_pEnergyForeground);
-    for (UINT iPixel = 0; iPixel < m_uiEnergyDisplayHeight; ++iPixel)
-    {
+    for (UINT iPixel = 0; iPixel < m_uiEnergyDisplayHeight; ++iPixel) {
         pPixels[iPixel] = 0x8A2BE2;
     }
 
@@ -303,30 +264,26 @@ HRESULT AudioPanel::CreateEnergyDisplay()
                 &m_pEnergyDisplay 
                 );
 
-    if (SUCCEEDED(hr))
-    {
+    if (SUCCEEDED(hr)) {
         m_pEnergyDisplay->CopyFromMemory(NULL, m_pEnergyBackground, m_uiEnergyBackgroundStride);
     }
 
     return hr;
 }
 
-/// <summary>
+
 /// Create gauge (with needle) used to display beam angle.
-/// </summary>
+
 /// <returns>S_OK on success, otherwise failure code.</returns>
-HRESULT AudioPanel::CreateBeamGauge()
-{
+HRESULT AudioPanel::CreateBeamGauge() {
     HRESULT hr = m_pD2DFactory->CreatePathGeometry(&m_pBeamGauge);
 
     // Create gauge background shape
-    if (SUCCEEDED(hr))
-    {
+    if (SUCCEEDED(hr)) {
         ID2D1GeometrySink *pGeometrySink = NULL;
         hr = m_pBeamGauge->Open(&pGeometrySink);
 
-        if (SUCCEEDED(hr))
-        {
+        if (SUCCEEDED(hr)) {
             pGeometrySink->BeginFigure(D2D1::Point2F(0.1503f,0.2832f), D2D1_FIGURE_BEGIN_FILLED);
             pGeometrySink->AddLine(D2D1::Point2F(0.228f,0.2203f));
             pGeometrySink->AddArc(D2D1::ArcSegment(D2D1::Point2F(0.772f,0.2203f), D2D1::SizeF(0.35f,0.35f), 102, D2D1_SWEEP_DIRECTION_COUNTER_CLOCKWISE, D2D1_ARC_SIZE_SMALL));
@@ -336,8 +293,7 @@ HRESULT AudioPanel::CreateBeamGauge()
             hr = pGeometrySink->Close();
 
             // Create gauge background brush
-            if (SUCCEEDED(hr))
-            {
+            if (SUCCEEDED(hr)) {
                 ID2D1GradientStopCollection *pGradientStops = NULL;
                 D2D1_GRADIENT_STOP gradientStops[4];
                 gradientStops[0].color = D2D1::ColorF(D2D1::ColorF::LightGray, 1);
@@ -354,12 +310,10 @@ HRESULT AudioPanel::CreateBeamGauge()
                     &pGradientStops
                     );
 
-                if (SUCCEEDED(hr))
-                {
+                if (SUCCEEDED(hr)) {
                     hr = m_pRenderTarget->CreateRadialGradientBrush(D2D1::RadialGradientBrushProperties(D2D1::Point2F(0.5f,0.0f), D2D1::Point2F(0.0f,0.0f), 1.0f, 1.0f), pGradientStops, &m_pBeamGaugeFill);
 
-                    if (SUCCEEDED(hr))
-                    {
+                    if (SUCCEEDED(hr)) {
                         // Create gauge needle shape and fill brush
                         hr = CreateBeamGaugeNeedle();
                     }
@@ -375,22 +329,19 @@ HRESULT AudioPanel::CreateBeamGauge()
     return hr;
 }
 
-/// <summary>
+
 /// Create gauge needle used to display beam angle.
-/// </summary>
+
 /// <returns>S_OK on success, otherwise failure code.</returns>
-HRESULT AudioPanel::CreateBeamGaugeNeedle()
-{
+HRESULT AudioPanel::CreateBeamGaugeNeedle() {
     HRESULT hr = m_pD2DFactory->CreatePathGeometry(&m_pBeamNeedle);
 
     // Create gauge needle shape
-    if (SUCCEEDED(hr))
-    {
+    if (SUCCEEDED(hr)) {
         ID2D1GeometrySink *pGeometrySink = NULL;
         hr = m_pBeamNeedle->Open(&pGeometrySink);
 
-        if (SUCCEEDED(hr))
-        { 
+        if (SUCCEEDED(hr)) { 
             pGeometrySink->BeginFigure(D2D1::Point2F(0.495f,0.35f), D2D1_FIGURE_BEGIN_FILLED);
             pGeometrySink->AddLine(D2D1::Point2F(0.505f,0.35f));
             pGeometrySink->AddLine(D2D1::Point2F(0.5f,0.44f));
@@ -398,8 +349,7 @@ HRESULT AudioPanel::CreateBeamGaugeNeedle()
             hr = pGeometrySink->Close();
 
             // Create gauge needle brush
-            if (SUCCEEDED(hr))
-            {
+            if (SUCCEEDED(hr)) {
                 ID2D1GradientStopCollection *pGradientStops = NULL;
                 D2D1_GRADIENT_STOP gradientStops[4];
                 gradientStops[0].color = D2D1::ColorF(D2D1::ColorF::LightGray, 1);
@@ -416,8 +366,7 @@ HRESULT AudioPanel::CreateBeamGaugeNeedle()
                     &pGradientStops
                     );
 
-                if (SUCCEEDED(hr))
-                {
+                if (SUCCEEDED(hr)) {
                     hr = m_pRenderTarget->CreateLinearGradientBrush(D2D1::LinearGradientBrushProperties(D2D1::Point2F(0.5f,0.0f), D2D1::Point2F(0.5f,1.0f)), pGradientStops, &m_pBeamNeedleFill);
                 }
 
@@ -431,22 +380,19 @@ HRESULT AudioPanel::CreateBeamGaugeNeedle()
     return hr;
 }
 
-/// <summary>
+
 /// Create gauge (with position cloud) used to display sound source angle.
-/// </summary>
+
 /// <returns>S_OK on success, otherwise failure code.</returns>
-HRESULT AudioPanel::CreateSourceGauge()
-{
+HRESULT AudioPanel::CreateSourceGauge() {
     HRESULT hr = m_pD2DFactory->CreatePathGeometry(&m_pSourceGauge);
 
     // Create gauge background shape
-    if (SUCCEEDED(hr))
-    {
+    if (SUCCEEDED(hr)) {
         ID2D1GeometrySink *pGeometrySink = NULL;
         hr = m_pSourceGauge->Open(&pGeometrySink);
 
-        if (SUCCEEDED(hr))
-        {
+        if (SUCCEEDED(hr)) {
             pGeometrySink->BeginFigure(D2D1::Point2F(0.1270f,0.3021f), D2D1_FIGURE_BEGIN_FILLED);
             pGeometrySink->AddLine(D2D1::Point2F(0.1503f,0.2832f));
             pGeometrySink->AddArc(D2D1::ArcSegment(D2D1::Point2F(0.8497f,0.2832f), D2D1::SizeF(0.45f,0.45f), 102, D2D1_SWEEP_DIRECTION_COUNTER_CLOCKWISE, D2D1_ARC_SIZE_SMALL));
@@ -456,8 +402,7 @@ HRESULT AudioPanel::CreateSourceGauge()
             hr = pGeometrySink->Close();
 
             // Create gauge background brush
-            if (SUCCEEDED(hr))
-            {
+            if (SUCCEEDED(hr)) {
                 hr = CreateSourceGaugeFill(0.1f);
             }
         }
@@ -468,16 +413,15 @@ HRESULT AudioPanel::CreateSourceGauge()
     return hr;
 }
 
-/// <summary>
+
 /// Create gradient used to represent sound source confidence, with the
 /// specified width.
-/// </summary>
+
 /// <param name="width">
 /// Width of gradient, specified in [0.0,1.0] interval.
 /// </param>
 /// <returns>S_OK on success, otherwise failure code.</returns>
-HRESULT AudioPanel::CreateSourceGaugeFill(const float & width)
-{
+HRESULT AudioPanel::CreateSourceGaugeFill(const float & width) {
     HRESULT hr = S_OK;
 
     float halfWidth = width / 2;
@@ -499,8 +443,7 @@ HRESULT AudioPanel::CreateSourceGaugeFill(const float & width)
         &pGradientStops
         );
 
-    if (SUCCEEDED(hr))
-    {
+    if (SUCCEEDED(hr)) {
         SafeRelease(m_pSourceGaugeFill);
         hr = m_pRenderTarget->CreateLinearGradientBrush(D2D1::LinearGradientBrushProperties(D2D1::Point2F(0.0f,0.5f), D2D1::Point2F(1.0f,0.5f)), pGradientStops, &m_pSourceGaugeFill);
     }
@@ -509,22 +452,19 @@ HRESULT AudioPanel::CreateSourceGaugeFill(const float & width)
     return hr;
 }
 
-/// <summary>
+
 /// Create outline that frames both gauges and energy display into a cohesive panel.
-/// </summary>
+
 /// <returns>S_OK on success, otherwise failure code.</returns>
-HRESULT AudioPanel::CreatePanelOutline()
-{
+HRESULT AudioPanel::CreatePanelOutline() {
     HRESULT hr = m_pD2DFactory->CreatePathGeometry(&m_pPanelOutline);
 
     // Create panel outline path
-    if (SUCCEEDED(hr))
-    {
+    if (SUCCEEDED(hr)) {
         ID2D1GeometrySink *pGeometrySink = NULL;
         hr = m_pPanelOutline->Open(&pGeometrySink);
 
-        if (SUCCEEDED(hr))
-        {
+        if (SUCCEEDED(hr)) {
             /// Draw left wave display frame
             pGeometrySink->BeginFigure(D2D1::Point2F(0.15f,0.0353f), D2D1_FIGURE_BEGIN_FILLED);
             pGeometrySink->AddLine(D2D1::Point2F(0.13f,0.0353f));
@@ -550,8 +490,7 @@ HRESULT AudioPanel::CreatePanelOutline()
             hr = pGeometrySink->Close();
 
             // Create panel outline brush
-            if (SUCCEEDED(hr))
-            {
+            if (SUCCEEDED(hr)) {
                 hr = m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::LightGray), &m_pPanelOutlineStroke);
             }
         }
